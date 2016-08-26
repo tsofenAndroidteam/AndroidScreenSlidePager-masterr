7052073ab106195dfa543fc44d140c940ca04bd6
Michaelg <shsmichael@gmail.com>
2016-08-26 14:44:52 +0300

    Changed BookIcon Updated StringArray of links

--- Sample/src/main/java/com/liangfeizc/screenslidepager/sample/MainActivity.java
+++ Sample/src/main/java/com/liangfeizc/screenslidepager/sample/MainActivity.java
@@ -14,11 +14,19 @@ import com.liangfeizc.slidepager.SlidePagerActivity;
 public class MainActivity extends ActionBarActivity {
 
     private static final String[] IMAGES = new String[] {
-            "http://img1.cache.netease.com/catchpic/B/B2/B2F274C1CCD5A89133261E6252A0C8E9.jpg",
-            "http://img5.duitang.com/uploads/item/201408/09/20140809204759_CwtQN.jpeg",
-            "http://img5q.duitang.com/uploads/item/201204/06/20120406151343_HefME.jpeg",
-            "http://images.qianlong.com/mmsource/imghylanda/201108/25/21/7131919880274907937.jpg",
-            "http://imgsrc.baidu.com/forum/pic/item/78370e46f91582056a63e576.jpg"
+
+            "http://iiif.nli.org.il/IIIF/FL13407243/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407246/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407245/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407153/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407216/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407202/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407227/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407202/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407108/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407109/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407121/full/full/0/default.jpg",
+            "http://iiif.nli.org.il/IIIF/FL13407218/full/full/0/default.jpg"
     };
 
 
